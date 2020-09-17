ebceaaca1e13d97ebd33a7d9ca92e315b69ee2d1
pyjpjw <yujin970521@naver.com>
2020-09-17 16:44:11 +0900

    20-09-17

---
+++ 20170757_PYJ/200917.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	float num1 = 3.e5f;
+	double num2 = -1.3827e-2;
+	long double num3 = 5.21e+91;
+
+	printf("%f %f %Lf\n", num1, num2, num3);
+	printf("%e %e %Le\n", num1, num2, num3);
+
+	return 0;
+}
